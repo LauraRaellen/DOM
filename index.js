458956f0e0002a3d7651a6261314197c608b636a
Laura Raellen <lauraraellen0@gmail.com>
2022-07-28 16:08:43 -0300

    innerText

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const element = document.querySelector('h1')
 
-element.textContent += " Olá Devs!"
+element.innerText += " Olá Devs!"
 
-console.log(element.textContent);
+console.log(element.innerText);
