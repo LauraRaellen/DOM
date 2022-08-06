6a34ffcaa7ffa8f2286b43a9763d70c52475120d
Laura Raellen <lauraraellen0@gmail.com>
2022-08-06 12:00:13 -0300

    Navegando nos elementos pate 1

---
+++ child/index.js
@@ -0,0 +1,9 @@
+//childNodes children
+const el = document.querySelector("body")
+// console.log(el.childNodes);
+
+//firstChild firstElementChild
+// console.log(el.firstElementChild);
+
+//lastChild lastElementChild
+console.log(el.lastElementChild);
